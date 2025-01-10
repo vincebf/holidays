cbd0853fd9ccab0d1e3e23ccac2e392da3403ab9
vincebf <3348173@qq.com>
2025-01-10 15:36:33 +0800

    Initial commit

--- holidays/weekholidays.py
+++ holidays/weekholidays.py
@@ -3,7 +3,7 @@ from config import public_holidays
 
 if __name__ == "__main__":
     current_week = public_holidays.is_weekday()
-    print(f"{current_week}")
+    # print(f"{current_week}")
     if current_week == 1:
         print(f"It's not the holidays and is the Friday!")
     else:
