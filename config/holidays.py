a6c8691a5f2b7b9a1b0050b24a4ab383c02a1be9
vincebf <3348173@qq.com>
2025-01-26 12:02:40 +0800

    one

---
+++ config/holidays.py
@@ -0,0 +1,99 @@
+from datetime import date, datetime
+import requests
+import json
+import sys
+import time
+from config import holidays
+import base64
+import hashlib
+
+url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=5210cec9-4bc5-470e-b541-1dd20b88280a"
+SELF = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=990a7ff5-6428-4b75-9afe-b995c150ca81"
+headers = {"content-type": "application/json"}
+
+holidays = [
+    "2025-01-01",  # 元旦
+    "2025-01-28",  # 春节
+    "2025-01-29",  # 春节
+    "2025-01-30",  # 春节
+    "2025-01-31",  # 春节
+    "2025-02-01",  # 春节
+    "2025-02-02",  # 春节
+    "2025-02-03",  # 春节
+    "2025-02-04",  # 春节
+    "2025-04-04",  # 清明节
+    "2025-04-05",  # 清明节
+    "2025-04-06",  # 清明节
+    "2025-05-01",  # 五一
+    "2025-05-02",  # 五一
+    "2025-05-03",  # 五一
+    "2025-05-04",  # 五一
+    "2025-05-05",  # 五一
+    "2025-05-31",  # 端午
+    "2025-06-01",  # 端午
+    "2025-06-02",  # 端午
+    "2025-10-01",  # 国庆
+    "2025-10-02",  # 国庆
+    "2025-10-03",  # 国庆
+    "2025-10-04",  # 国庆
+    "2025-10-05",  # 国庆
+    "2025-10-06",  # 国庆
+    "2025-10-07",  # 国庆
+    "2025-10-08",  # 国庆
+]
+
+
+def data_image(filepath):
+    with open(filepath, "rb") as f:
+        img_data = f.read()
+        base64_image = base64.b64encode(img_data).decode("utf-8")
+        md5_res = hashlib.md5(img_data).hexdigest()
+
+    message_data = {
+        "msgtype": "image",
+        "image": {"base64": base64_image, "md5": md5_res},
+    }
+    data2 = json.dumps(message_data)
+    return data2
+
+
+def data1(message):
+    data = {
+        "msgtype": "text",
+        "text": {
+            "content": message,
+            "mentioned_list": ["@all"],
+        },
+    }
+    data = json.dumps(data)
+    return data
+
+
+def data2(message1):
+    data = {
+        "msgtype": "text",
+        "text": {
+            "content": message1,
+            "mentioned_list": ["@all"],
+        },
+    }
+    data = json.dumps(data)
+    return data
+
+
+def is_weekday():
+    # 获取当前日期
+    today = datetime.today().date()
+    # 检查是否是周末
+    if today.weekday() == 4:
+        if today.strftime("%Y-%m-%d") not in holidays:
+            return 1
+    return 2
+
+
+if __name__ == "__main__":
+    current_week = is_weekday()
+    if current_week == 1:
+        print(f"ok!")
+    else:
+        print(f"not ok!")
---
+++ is_weekholidays.py
@@ -0,0 +1,37 @@
+from lunarcalendar import Converter, Solar, Lunar
+from datetime import date, datetime
+from config import holidays
+import requests
+import hashlib
+import base64
+import time
+import json
+import sys
+
+
+# 获取当前年份
+current_year = date.today().year
+
+# 计算春节日期（农历正月初一）
+lunar_new_year = Lunar(current_year, 1, 1)
+solar_new_year = Converter.Lunar2Solar(lunar_new_year)
+# 计算距离春节的天数
+days_until_spring_festival = (solar_new_year.to_date() - date.today()).days
+print(f"距离春节还有 {days_until_spring_festival} 天")
+
+message1 = "大扫除"
+message2 = "不是大扫除日"
+filepath = "C:/Users/wuzheng3/Pictures/dsc.jpeg"
+data = holidays.data1(message1)
+data1 = holidays.data2(message2)
+data2 = holidays.data_image(filepath)
+
+
+if __name__ == "__main__":
+    current_week = holidays.is_weekday()
+    if current_week == 1:
+        print(requests.post(url=holidays.SELF, headers=holidays.headers, data=data1))
+        if days_until_spring_festival <= 7:
+            print(
+                requests.post(url=holidays.SELF, headers=holidays.headers, data=data2)
+            )
